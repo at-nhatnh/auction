cf78c451de91bbfa160686affe050ae8ccee5c3e
NhatNH <nhatnh@asiantech.vn>
2016-10-11 10:16:35 +0700

    add_carrierwave

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id         :integer          not null, primary key
+#  image_url  :string(255)
+#  item_id    :integer
+#  item_type  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
